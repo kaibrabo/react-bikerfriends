78298f74b5085ae255ed0b5f79781e1713e2994e
Kai Brabo <kaibrabo@gmail.com>
2018-11-13 18:23:15 -0800

    add searchbox, create search func 'filteredFriends'

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 import CardList from './CardList';
 import { friends } from './friends';
 import SearchBox from './SearchBox';
 
-const App = () => {
-    return (
-        <div className='tc'>
-            <h1>Biker Friends</h1>
-            <SearchBox />
-            <CardList friends={friends} />
-        </div>
-    );
+
+class App extends Component {
+    
+    constructor() {
+        super()
+        this.state = {
+            friends: friends,
+            searchfield: ''
+        }
+    }
+
+    onSearchChange = (event) => {
+        this.setState({ searchfield: event.target.value })
+
+        console.log(event.target.value)
+    }
+
+    render() {
+        const filteredFriends = this.state.friends.filter(
+            friends => {
+                return friends.name.toLowerCase().includes(
+                    this.state.searchfield.toLowerCase()
+                );
+            }
+        )
+
+        return (
+            <div className='tc'>
+                <h1>Biker Friends</h1>
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList friends={filteredFriends} />
+            </div>
+        );
+    }
+
 }
 
 export default App;
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({seachfield, searchChange}) => {
     return (
         <div className='pa2'>
             <input
                 className='pa3 ba b--blue'
                 type='search'
-                placeholder='Search Friends'>
+                placeholder='Search Friends'
+                onChange={searchChange}>
             </input>
         </div>
     );
