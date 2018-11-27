2786224beddca55b043daa6dfc089a23964d4a84
Kai Brabo <kaibrabo@gmail.com>
2018-11-27 11:36:33 -0800

    update initialStateSearch, initialStateFriends

--- src/reducers.js
+++ src/reducers.js
@@ -1,14 +1,44 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_FRIENDS_PENDING,
+    REQUEST_FRIENDS_SUCCESS,
+    REQUEST_FRIENDS_FAILED
+} from "./constants";
 
-const initialState = {
-    searchField: ''
+const initialStateSearch = {
+    searchField: ""
+};
+
+const initialStateFriends = {
+    friends: [],
+    isPending: false,
+    error: ''
 }
 
-export const searchFriends = (state=initialState, action={}) => {    
-    switch(action.type) {
+export const searchFriends = (state = initialStateSearch, action = {}) => {
+    switch (action.type) {
         case CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, {searchField: action.payload});
+            return Object.assign({}, state, { searchField: action.payload });
         default:
             return state;
     }
-}
+};
+
+export const requestFriends = (state = initialStateFriends, action = {}) => {
+    switch (action.type) {
+        case REQUEST_FRIENDS_PENDING:
+            return Object.assign({}, state, { isPending: true });
+        case REQUEST_FRIENDS_SUCCESS:
+            return Object.assign({}, state, {
+                friends: action.payload,
+                isPending: false
+            });
+        case REQUEST_FRIENDS_FAILED:
+            return Object.assign({}, state, {
+                error: action.payload,
+                isPending: false
+            });
+        default:
+            return state;
+    }
+};
