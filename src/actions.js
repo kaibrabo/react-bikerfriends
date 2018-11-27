13631cd54e53e39545c6f1ef974b878a7b59bebb
Kai Brabo <kaibrabo@gmail.com>
2018-11-27 11:21:40 -0800

    create requestFriends() in actions.js

--- src/actions.js
+++ src/actions.js
@@ -10,3 +10,15 @@ export const setSearchField = text => ({
     type: CHANGE_SEARCH_FIELD,
     payload: text
 });
+
+export const requestFriends = dispatch => {
+    dispatch({ type: REQUEST_FRIENDS_PENDING });
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(data => dispatch({ 
+            type: REQUEST_FRIENDS_SUCCESS,
+            payload: data
+        }))
+        .catch(error => dispatch({ type: REQUEST_FRIENDS_FAILED }));
+};
