b7b39d41401ae415647ea0063c3713eea2cc3084
Kai Brabo <kaibrabo@gmail.com>
2018-11-27 11:14:49 -0800

    add constants to actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,7 +1,12 @@
-import { CHANGE_SEARCH_FIELD } from './constants';
+import {
+    CHANGE_SEARCH_FIELD,
+    REQUEST_FRIENDS_PENDING,
+    REQUEST_FRIENDS_SUCCESS,
+    REQUEST_FRIENDS_FAILED
+} from "./constants";
 
-// {return {}} == ({})
-export const setSearchField = (text) => ({
-        type: CHANGE_SEARCH_FIELD,
-        payload: text
-})
+// ES6 - {return {}} == ({})
+export const setSearchField = text => ({
+    type: CHANGE_SEARCH_FIELD,
+    payload: text
+});
